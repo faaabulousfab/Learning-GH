55b04f1929ac26cb4b44b6b2645f4d3b90d950df
Fabien Azra <fabien.azra@me.com>
2023-04-13 18:38:47 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Fabien AZRA on 13/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
